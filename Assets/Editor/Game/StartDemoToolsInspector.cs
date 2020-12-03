d1c3bd086cf7ed8696777f6bb6059edb6ae0ac7f
marstau <taumars@hotmail.com>
2020-12-03 18:54:40 +0800

    -

---
+++ Assets/Editor/Game/StartDemoToolsInspector.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(StartDemo))]
+public class StartDemoToolsInspector : Editor
+{
+    private StartDemo _getStartDemo { get { return target as StartDemo; } }
+    private bool isload = false;
+    private bool isobjview = false;
+    private bool istexview = false;
+
+
+    private ConfigTeamParams configTeamA;
+    private ConfigTeamParams configTeamB;
+
+    private void initConfig() {
+        _initConfig(configTeamA, "A");
+        _initConfig(configTeamB, "B");
+    }
+    private void _initConfig(ConfigTeamParams configTeam, string str) {
+        if (configTeam == null) {
+            init();
+            return;
+        }
+        EditorGUILayout.BeginHorizontal();
+        // EditorGUILayout.Space();
+        EditorGUILayout.LabelField("部队"+str+"配置参数:",GUILayout.Width(160));
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        configTeam.show = EditorGUILayout.Toggle("出现：", configTeam.show);
+        EditorGUILayout.EndHorizontal();
+
+        // if (showA)
+        //     _getStartDemo.setBattleState(2);
+        // else
+        //     _getStartDemo.setBattleState(0);
+
+        EditorGUILayout.BeginHorizontal();
+        // configTeam.move = EditorGUILayout.Toggle("移动：", configTeam.move);
+        configTeam.move = (ROLE_STATE)EditorGUILayout.EnumPopup("动作：", configTeam.move);
+        // if (GUILayout.Button("Create"))
+        //     InstantiatePrimitive(configTeam.move);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        configTeam.startPos = EditorGUILayout.Vector3Field("起点：", configTeam.startPos);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        configTeam.endPos = EditorGUILayout.Vector3Field("终点：", configTeam.endPos);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        configTeam.number = EditorGUILayout.IntField("数量：", configTeam.number);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        configTeam.teamRect = EditorGUILayout.Vector2Field("阵容范围(长宽)：", configTeam.teamRect);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        configTeam.formation = (FORMATION)EditorGUILayout.EnumPopup("阵容：", configTeam.formation);
+        EditorGUILayout.EndHorizontal();
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("reload:",GUILayout.Width(105));
+        this.isload = EditorGUILayout.Toggle(this.isload);
+        if (this.isload){
+            base.OnInspectorGUI();
+        } else {
+            init();
+        }
+
+        initConfig();
+        Repaint();
+    }
+
+    private void init() {
+        Debug.Log("OnInspectorGUI Init=" + this.isload);
+        _getStartDemo.Init();
+        configTeamA = _getStartDemo.getConfigTeamA();
+        configTeamB = _getStartDemo.getConfigTeamB();
+        this.isload = true;
+    }
+
+    //  void _lithgmeshgui(){
+    //     LightingMapDataInfo[] aa = _getStartDemo.get_renderinfo();
+    //     for (int i = 0 ; i < aa.Length; i ++){
+    //         EditorGUILayout.BeginVertical();
+    //             // EditorGUILayout.LabelField("["+i.ToString()+"]",GUILayout.Width(20));
+    //             EditorGUILayout.BeginHorizontal();
+    //                 EditorGUILayout.LabelField("["+aa[i].lightindex.ToString()+"]Render:",GUILayout.Width(65));
+    //                 // EditorGUILayout.LabelField("color",GUILayout.Width(20));
+    //                 switch(aa[i].type){
+    //                     case 1:
+    //                         EditorGUILayout.ObjectField(aa[i].meshrender,typeof(MeshRenderer),false);
+    //                     break;
+    //                     case 2:
+    //                         EditorGUILayout.ObjectField(aa[i].terrain,typeof(Terrain),false);
+    //                     break;
+    //                 }
+                   
+                    
+    //                 // EditorGUILayout.LabelField("Dir",GUILayout.Width(20));
+    //                 // EditorGUILayout.ObjectField(aa[i].lightmapDir,typeof(Texture2D),false);
+    //             EditorGUILayout.EndHorizontal();
+    //         EditorGUILayout.EndVertical();
+    //     }
+
+    // }
+    // void _lithgdatagui(){
+    //     Texture2D[] aa = _getStartDemo.get_renderDatainfo().ToArray();
+    //     for (int i = 0 ; i < aa.Length; i ++){
+    //         EditorGUILayout.BeginVertical();
+    //             // EditorGUILayout.LabelField("["+i.ToString()+"]",GUILayout.Width(20));
+    //             EditorGUILayout.BeginHorizontal();
+    //                 EditorGUILayout.LabelField("["+i.ToString()+"]color:",GUILayout.Width(65));
+    //                 // EditorGUILayout.LabelField("color",GUILayout.Width(20));
+    //                 EditorGUILayout.ObjectField(aa[i],typeof(Texture2D),false);
+    //                 // EditorGUILayout.LabelField("Dir",GUILayout.Width(20));
+    //                 // EditorGUILayout.ObjectField(aa[i].lightmapDir,typeof(Texture2D),false);
+    //             EditorGUILayout.EndHorizontal();
+    //         EditorGUILayout.EndVertical();
+    //     }
+
+    // }
+
+
+}
--- Assets/GameScripts/Game/StartDemo.cs
+++ Assets/GameScripts/Game/StartDemo.cs
@@ -2,21 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ROLE_STATE
+{
+    IDLE = 0,
+    ATTACK = 1,
+    DIE = 2,
+    JUMP_DIE = 3,
+}
+
+
+public enum ROLE_SHOW_STATE
+{
+    HIDE = 0,
+    SHOW = 1,
+}
+
+public enum FORMATION {
+    CIRCLE = 0,
+    RECT = 1,
+    TRIANGLE = 2,
+    ARROW = 3,
+}
+
+public class ConfigTeamParams {
+    public bool show;
+    public ROLE_STATE move;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public double moveSpeed;
+    public int number;
+    public Vector2 teamRect;
+    public FORMATION formation;
+}
+
+public enum GAME_STATE {
+	READY = 1,
+	START = 2,
+	DURATION = 3,
+	END = 4,
+}
+
+public class Team {
+}
+
 public class StartDemo : MonoBehaviour
 {
+	private GAME_STATE _gameState;
+    private ConfigTeamParams configTeamA;
+    private ConfigTeamParams configTeamB;
+
     // Start is called before the first frame update
     void Start()
     {
+        _gameState = GAME_STATE.READY;
+    }
+
+    void initBattleInfo(){
         GameObject role = (GameObject)Resources.Load("Model/model1001001/model1001001");
         GameObject roleNode1 = GameObject.Find("Terrain/RoleNode1");
         role = Instantiate(role);
         role.transform.parent = roleNode1.transform;
         role.transform.localPosition = new Vector3(0, 0, 0);
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+    	switch (_gameState) {
+    		case GAME_STATE.READY:
+    		if (configTeamA != null && configTeamA.show) {
+    			changeState(GAME_STATE.START);
+    		}
+    		break;
+
+    		case GAME_STATE.START:
+    		initBattleInfo();
+    		break;
+
+    		case GAME_STATE.DURATION:
+    		break;
+
+    		case GAME_STATE.END:
+    		break;
+    	}
+    	if (configTeamA != null)
+        	Debug.Log("configTeamA=" + configTeamA.show + ", configTeamB=" + configTeamB.number);
+        else
+        	Debug.Log("configTeamA");
+    }
+
+    public void changeState(GAME_STATE state) {
+    	_gameState = state;
+    }
+    public void Init() {
+    	configTeamA = new ConfigTeamParams();
+    	configTeamB = new ConfigTeamParams();
+    }
+
+    public ConfigTeamParams getConfigTeamA() {
+    	return configTeamA;
+    }
+    public ConfigTeamParams getConfigTeamB() {
+    	return configTeamB;
     }
 }
