896c2eabef6ea67a8476780192797891ca47a016
marstau <taumars@hotmail.com>
2020-12-04 18:11:24 +0800

    -

--- Assets/GameScripts/Game/StartDemo.cs
+++ Assets/GameScripts/Game/StartDemo.cs
@@ -48,6 +48,8 @@ public class Role {
 		_obj = obj;
 
 	}
+
+	public GameObject getGo() { return _obj; }
 }
 
 public class StartDemo : MonoBehaviour
@@ -73,6 +75,7 @@ public class StartDemo : MonoBehaviour
         	case FORMATION.RECT:
         	int width = _config.teamRect.x;
         	int height = _config.teamRect.y;
+        	Vector3 tarPos = new Vector3(1.0f, 0.0f, 0.0f);
         	for (int x = 0; x < width; x++) {
         		List<Role> colRoles = new List<Role>();
         		instances.Add(colRoles);
@@ -82,11 +85,13 @@ public class StartDemo : MonoBehaviour
 			        go = Instantiate(go);
 			        go.transform.parent = _roleNode1.transform;
 			        go.transform.localPosition = new Vector3(x, 0, y);
+			        go.transform.Rotate(0, 180, 0, Space.Self);
 		            ModelCustomData soldier_model_custom_data = go.GetComponent<ModelCustomData>();
-		            soldier_model_custom_data.setPower(2);
+		            soldier_model_custom_data.setPower(1);
 		            soldier_model_custom_data.setLightDir(new Vector4(1.42f, 3.16f, 1.48f, 1.0f));
 		            soldier_model_custom_data.setShadowColor(new Color(0.608f, 0.608f, 0.608f, 1f));
 		            soldier_model_custom_data.getAnimator().Play("idle", 0, 0);
+			        // soldier_model_custom_data.getAnimator().SetLookAtPosition(tarPos);
 			        Role role = new Role(go);
 			        colRoles.Add(role);
         		}
@@ -124,6 +129,7 @@ public class StartDemo : MonoBehaviour
     		break;
 
     		case GAME_STATE.DURATION:
+    			loopGame();
     		break;
 
     		case GAME_STATE.END:
@@ -156,5 +162,47 @@ public class StartDemo : MonoBehaviour
     }
     public ConfigTeamParams getConfigTeamB() {
     	return _configTeamB;
+    }
+
+    private void loopGame() {
+        Vector3 tarPos = new Vector3(-1.0f, 0.0f, -1.0f);
+
+    	Vector3 TargetPosition = new Vector3(1000,0,1000);
+		float AvatarRange = 25;
+		Debug.Log("_instancesA=" + _instancesA);
+		Debug.Log("_instancesA size=" + _instancesA.Count);
+    	foreach (List<Role> colRoles in _instancesA) {
+    		foreach (Role role in colRoles) {
+    			GameObject go = role.getGo();
+    			Transform transform = go.transform;
+	      //       if (Vector3.SqrMagnitude(TargetPosition - transform.position) > 25)
+	      //       {
+
+	      //           //avatar.SetFloat("Speed", 1, SpeedDampTime, Time.deltaTime);
+
+	      //           Vector3 curentDir = transform.rotation * Vector3.forward;
+	                Vector3 wantedDir = (TargetPosition - transform.position).normalized;
+	                Debug.Log("wantedDir=" + wantedDir);
+	      //           transform.Translate(wantedDir * 1.0f * Time.deltaTime);
+	      //           transform.rotation.SetLookRotation(wantedDir);
+	                // go.transform.Rotate(0, 180, 0, Space.Self);
+	      //           go.transform.rotation = Quaternion.LookRotation(TargetPosition - transform.position);
+	      //           // transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(TargetPosition - transform.position), 8.0f);
+	      //       }
+	      //       else
+	      //       {
+
+	      //           //if (avatar.GetFloat("Speed") < 0.01f)
+	      //           {
+	      //               // instancing.PlayAnimation(UnityEngine.Random.Range(0, 2));
+	      //               //instancing.PlayAnimation(1);
+	      //               //instancing.CrossFade(1, 0.1f);
+	      //               TargetPosition = new Vector3(UnityEngine.Random.Range(-AvatarRange, AvatarRange), 0, UnityEngine.Random.Range(-AvatarRange, AvatarRange));
+	      //               transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(TargetPosition - transform.position), 0.1f);
+	      //               //gameObject.transform.rotation = Quaternion.LookRotation(TargetPosition - transform.position);
+	      //           }
+	      //       }
+    		}
+    	}
     }
 }
